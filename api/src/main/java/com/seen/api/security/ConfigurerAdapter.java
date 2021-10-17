139c5edbde1af6dc99fcfc733c761f2c11f341ce
DESKTOP-MS49C4Q\Sabin Theodor <popescusabintheodor@gmail.com>
2021-10-17 23:00:24 +0300

    SEEN-9 Create basic security flow
    Allows /home to be accessed.

---
+++ api/src/main/java/com/seen/api/security/ConfigurerAdapter.java
@@ -0,0 +1,21 @@
+package com.seen.api.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Configuration of {@link WebSecurity}.
+ * 
+ * @author Sabin Theodor
+ *
+ */
+@Configuration
+public class ConfigurerAdapter extends WebSecurityConfigurerAdapter {
+	
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/home").permitAll().anyRequest().authenticated();
+  }
+}
