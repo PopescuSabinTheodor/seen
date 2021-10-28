db8bb9eedd734610739aca6f3415f0638b5318ad
DESKTOP-MS49C4Q\Sabin Theodor <popescusabintheodor@gmail.com>
2021-10-28 23:28:20 +0300

    SEEN-12 Create client

---
+++ seen-client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { hot } from "react-hot-loader/root";
+
+const render = (Component) =>
+  ReactDOM.render(<Component />, document.getElementById("root"));
+
+render(hot(App));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
