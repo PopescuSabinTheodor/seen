aeca663e7e59f7c2d8fc9cf75f956d8c0e7135b3
DESKTOP-MS49C4Q\Sabin Theodor <popescusabintheodor@gmail.com>
2021-11-14 12:17:41 +0200

    Add lombok builder

--- api/src/main/java/com/seen/api/movie/MovieConverter.java
+++ api/src/main/java/com/seen/api/movie/MovieConverter.java
@@ -1,22 +1,16 @@
 package com.seen.api.movie;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MovieConverter {
 
-    @Autowired
-    private Movie.MovieBuilder movieBuilder;
-
     public Movie toMovie(MovieDto movieDto) {
-        Movie movie = movieBuilder
+        return Movie.builder()
                 .title(movieDto.getTitle())
                 .description(movieDto.getDescription())
                 .releaseDate(movieDto.getReleaseDate())
                 .build();
-        return movie;
-
     }
 
 }
